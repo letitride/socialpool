b0393a23c5f095a56f8d680db9e7eb9808238a11
letitride <ichikawa.fumiya@gmail.com>
2020-07-05 22:30:08 +0900

    5.3.2 MongoDBからの読み込み

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
 func main() {
-	setupTwitterAuth()
 }
