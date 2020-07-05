e1f3736e8f4bf03804420b6f82644153417f3307
letitride <ichikawa.fumiya@gmail.com>
2020-07-05 22:55:23 +0900

    5.3.3 twitterからの読み込み

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -19,5 +19,19 @@ func closedb() {
 	log.Println("データベース接続が閉じられました")
 }
 
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
 func main() {
 }
